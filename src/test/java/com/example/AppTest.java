ef81edbf3c25663ba28347079c9bedd58149540e
ganapathi6008 <kganapathi6008@gmail.com>
2025-02-21 17:57:13 +0000

    added test details

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,12 +1,6 @@
-package com.example;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
+import org.springframework.core.env.Environment;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AppTest {
@@ -17,6 +11,9 @@ class AppTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private Environment environment;
+
     @Test
     void contextLoads() {
         assertThat(restTemplate).isNotNull();
@@ -24,7 +21,10 @@ class AppTest {
 
     @Test
     void helloEndpointShouldReturnHelloWorld() {
-        String response = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+        // Read context path from application properties
+        String contextPath = environment.getProperty("server.servlet.context-path", "");
+        String response = this.restTemplate.getForObject("http://localhost:" + port + contextPath + "/", String.class);
         assertThat(response).isEqualTo("Hello, World from Spring Boot!");
     }
 }
+
